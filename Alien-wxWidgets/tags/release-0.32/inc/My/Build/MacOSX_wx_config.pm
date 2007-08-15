4e2b3e1d004d6f985442945bfcfec2c7d8fe1d0c
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-08-15 22:21:29 +0000

    Tag release 0.32.

---
+++ Alien-wxWidgets/tags/release-0.32/inc/My/Build/MacOSX_wx_config.pm
@@ -0,0 +1,39 @@
+package My::Build::MacOSX_wx_config;
+
+use strict;
+use base qw(My::Build::Any_wx_config);
+
+sub awx_wx_config_data {
+    my $self = shift;
+    return $self->{awx_data} if $self->{awx_data};
+    my %data = %{$self->SUPER::awx_wx_config_data};
+
+    # MakeMaker does not like some options
+    $data{libs} =~ s{-framework\s+\w+}{}g;
+    $data{libs} =~ s{-isysroot\s+\S+}{}g;
+    $data{libs} =~ s{-L/usr/local/lib\s}{}g;
+
+    $data{libs} =~ s{\s(-arch\s+\w+)}
+                    {$data{linkflags} .= " $1 ";
+                     $data{cxxflags} .= " $1 ";
+                     ' '}eg;
+
+    $data{ld} = $data{cxx};
+    $data{cxxflags} .= ' -UWX_PRECOMP ';
+
+    $self->{awx_data} = \%data;
+}
+
+sub awx_configure {
+    my $self = shift;
+    my %config = $self->SUPER::awx_configure;
+
+    $config{link_flags} .= $self->wx_config( 'linkflags' );
+
+    return %config;
+}
+
+sub awx_build_toolkit { 'mac' }
+sub awx_dlext { 'dylib' }
+
+1;
