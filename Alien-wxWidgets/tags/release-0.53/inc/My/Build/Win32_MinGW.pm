5d26cbd6ea22a03e07cce0f0598e14379e20a1b7
mdootson <mdootson@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2011-10-13 07:12:24 +0000

    Tagging Alien::wxWidgets 0.53 Release

---
+++ Alien-wxWidgets/tags/release-0.53/inc/My/Build/Win32_MinGW.pm
@@ -0,0 +1,141 @@
+package My::Build::Win32_MinGW;
+
+use strict;
+use base qw(My::Build::Win32);
+use My::Build::Utility qw(awx_arch_file awx_install_arch_file
+                          awx_install_arch_dir awx_arch_dir);
+use Config;
+use File::Basename qw();
+use File::Glob qw(bsd_glob);
+
+sub _find_make {
+    my( @try ) = qw(mingw32-make gmake make);
+    push @try, $Config{gmake} if $Config{gmake};
+
+    foreach my $name ( @try ) {
+        foreach my $dir ( File::Spec->path ) {
+            my $abs = File::Spec->catfile( $dir, "$name.exe" );
+            return $name if -x $abs;
+        }
+    }
+
+    return 'make';
+}
+
+sub awx_configure {
+    my $self = shift;
+    my %config = $self->SUPER::awx_configure;
+
+	my $mxarchflags = ( $Config{ptrsize} == 8 ) ? '-m64' : '-m32';
+
+    if( $self->awx_debug ) {
+        $config{c_flags} .= qq( -g $mxarchflags );
+        $config{link_flags} .= qq( $mxarchflags );
+    } else {
+        $config{c_flags} .= qq( $mxarchflags );
+        $config{link_flags} .= qq( -s $mxarchflags );
+    }
+
+    my $cccflags = $self->wx_config( 'cxxflags' );
+    my $libs = $self->wx_config( 'libs' );
+    my $incdir = $self->awx_wx_config_data->{wxinc};
+    my $cincdir = $self->awx_wx_config_data->{wxcontrinc};
+    my $iincdir = awx_install_arch_dir( $self, 'rEpLaCe/include' );
+
+    foreach ( split /\s+/, $cccflags ) {
+        m(^-DSTRICT) && next;
+        m(^\.d$) && next; # broken makefile
+        m(^-W.*) && next; # under Win32 -Wall gives you TONS of warnings
+        m(^-I) && do {
+            next if m{(?:regex|zlib|jpeg|png|tiff)$};
+            if( $self->notes( 'build_wx' ) ) {
+                $_ =~ s{\Q$cincdir\E}{$iincdir};
+                $_ =~ s{\Q$incdir\E}{$iincdir};
+            }
+            if( $_ =~ /-I\Q$self->{awx_setup_dir}\E/ ) {
+                $config{include_path} .=
+                  '-I' . awx_install_arch_file( $self, 'rEpLaCe/lib' ) . ' ';
+            } else {
+                $config{include_path} .= "$_ ";
+            }
+            next;
+        };
+        m(^-D) && do { $config{defines} .= "$_ "; next; };
+        $config{c_flags} .= "$_ ";
+    }
+
+    foreach ( split /\s+/, $libs ) {
+        m(wx|unicows)i || next;
+        next if m{(?:wx(?:zlib|regexu?|expat|png|jpeg|tiff)[ud]{0,2})$};
+        $config{link_libraries} .= "$_ ";
+    }
+
+    my $dlls = $self->awx_wx_config_data->{dlls};
+    $config{_libraries} = {};
+
+    while( my( $key, $value ) = each %$dlls ) {
+        $config{_libraries}{$key} =
+          { map { $_ => File::Basename::basename( $value->{$_} ) }
+                keys %$value };
+        if( $value->{link} ) {
+            $config{_libraries}{$key}{link} = $value->{link};
+        } elsif( $value->{lib} ) {
+            my $lib = $config{_libraries}{$key}{lib};
+            $lib =~ s/^lib(.*?)(?:\.dll)?\.a$/$1/;
+            $config{_libraries}{$key}{link} = '-l' . $lib;
+        }
+    }
+
+    return %config;
+}
+
+sub awx_compiler_kind { 'gcc' }
+
+sub files_to_install {
+    my $self = shift;
+    my( @try ) = qw(libgcc_*.dll mingwm10.dll);
+    my( $dll, $dll_from );
+    
+    # TODO: we need to determine this by testing one of
+    # the build DLL's
+    #
+    # A mingw build may link as static or dynamic to libgcc
+    # and libstdc++
+
+    foreach my $d ( @try ) {
+        $dll_from = $self->awx_path_search( $d );
+        if( defined $dll_from ) {
+            $dll = File::Basename::basename( $dll_from );
+            last;
+        }
+    }
+    
+    if(!defined($dll)) {
+        # check for special case ActivePerl mingw PPM
+        my $ppmmingw = qq($Config{sitearch}/auto/MinGW/bin/mingwm10.dll);
+        if( -f $ppmmingw ) {
+            $dll_from = $ppmmingw;
+            $dll = File::Basename::basename( $dll_from );
+        }
+    }
+    
+    if( defined( $dll_from ) && defined( $dll ) ) {
+        return ( $self->SUPER::files_to_install(),
+             ( $dll_from => awx_arch_file( "rEpLaCe/lib/$dll" ) ) );
+    } else {
+        die 'Cannot find libc (mingwm10.dll/ libgcc_*.dll) for install';
+    }
+
+}
+
+sub awx_strip_dlls {
+    my( $self ) = @_;
+    my( $dir ) = grep !/Config/, bsd_glob( awx_arch_dir( '*' ) );
+
+    $self->_system( "attrib -r $dir\\lib\\*.dll" );
+    $self->_system( "strip $dir\\lib\\*.dll" );
+    $self->_system( "attrib +r $dir\\lib\\*.dll" );
+}
+
+
+1;
